fd42136e03d7aaa3ed699bbbb6c5bc8d1499c2e3
martin <martin@ubuntu-portable>
2016-01-05 21:02:36 +0800

    simulate
    计分方式变更；
    增加规则文案；

--- captcha.parent/captcha.service/src/main/java/demo/captcha/util/ScoreGen.java
+++ captcha.parent/captcha.service/src/main/java/demo/captcha/util/ScoreGen.java
@@ -17,41 +17,27 @@ public class ScoreGen {
 		public int score(CaptchaExamClient client){
 			
 			int total = client.getTotal();
-			if(total < 200)
-				return 0;
-			if(total < 500)
-				return 65;
-			if(total < 1000)
-				return 70;
-			if(total < 2000)
-				return 75;
-			if(total < 3000)
-				return 80;
-			if(total < 4000)
-				return 85;
-			if(total < 5000)
-				return 90;
-			if(total < 6000)
-				return 95;
-			return 100;
+			int minus = total >= 1000 ? 0 : total - 1000;
+			minus = minus / 20;
+			return (100 + minus) < 55 ? 0 : (100 + minus);
 		}
 		
 		@Override
 		public boolean arriveAverage(CaptchaExamClient client){
-			return client.getTotal() >= 1000;
+			return client.getTotal() >= 900;
 		}
 	}
 	
 	static public class RateScore implements IScore{
 		@Override
 		public int score(CaptchaExamClient client){
-			int scoreRate = (int)client.getCorrectRate() > 95 ? (int)client.getCorrectRate() : 0;
+			int scoreRate = (int)client.getCorrectRate() > 89 ? (int)client.getCorrectRate() : 0;
 			return scoreRate;
 		}
 
 		@Override
 		public boolean arriveAverage(CaptchaExamClient client) {
-			return (int)client.getCorrectRate() > 97;
+			return (int)client.getCorrectRate() > 95;
 		}
 	}
 	
@@ -59,24 +45,9 @@ public class ScoreGen {
 		@Override
 		public int score(CaptchaExamClient client){
 			
-			int keyAvgCost = (int)client.getAvgCost()/1000;
-			if(keyAvgCost < 3)
-				return 100;
-			if(keyAvgCost < 4)
-				return 95;
-			if(keyAvgCost < 5)
-				return 90;
-			if(keyAvgCost<6)
-				return 85;
-			if(keyAvgCost<7)
-				return 80;
-			if(keyAvgCost<8)
-				return 75;
-			if(keyAvgCost<9)
-				return 70;
-			if(keyAvgCost<10)
-				return 65;
-			return 60;
+			int keyAvgCost = (int)client.getAvgCost()/100;
+			int minus = keyAvgCost <= 30 ? 0 : (30 - keyAvgCost)/2;
+			return (100 + minus)<65?0:(100 + minus);
 		}
 
 		@Override
@@ -87,40 +58,9 @@ public class ScoreGen {
 		}
 	}
 	
-	static Map<Integer, Integer> CaptchaAvgCost;
-	static Map<Integer, Integer> CaptchaTotal;
 	static Map<Integer, String> comments;
 	
 	static {
-		CaptchaAvgCost = new HashMap<Integer, Integer>();
-		CaptchaAvgCost.put(0, 100);
-		CaptchaAvgCost.put(1, 100);
-		CaptchaAvgCost.put(2, 100);
-		CaptchaAvgCost.put(3, 95);
-		CaptchaAvgCost.put(4, 90);
-		CaptchaAvgCost.put(5, 85);
-		CaptchaAvgCost.put(6, 80);
-		CaptchaAvgCost.put(7, 75);
-		CaptchaAvgCost.put(8, 70);
-		CaptchaAvgCost.put(9, 65);
-		
-		CaptchaTotal = new HashMap<Integer, Integer>();
-		CaptchaTotal.put(60, 100);
-		CaptchaTotal.put(50, 95);
-		CaptchaTotal.put(40, 90);
-		CaptchaTotal.put(30, 85);
-		CaptchaTotal.put(20, 80);
-		CaptchaTotal.put(10, 75);
-		//小于1000,70分
-		CaptchaTotal.put(9, 70);
-		CaptchaTotal.put(8, 70);
-		CaptchaTotal.put(7, 70);
-		CaptchaTotal.put(6, 70);
-		CaptchaTotal.put(5, 70);
-		//小于500,65分
-		CaptchaTotal.put(4, 65);
-		CaptchaTotal.put(3, 65);
-		CaptchaTotal.put(2, 65);
 		
 		comments = new HashMap<Integer, String>();
 		comments.put(0, "还没进入状态？要努力练习才不会有遗憾！");
@@ -173,9 +113,9 @@ public class ScoreGen {
 		//client.setCorrectRate((float)96.299034);
 		//client.setTotal(1351);
 		
-		client.setAvgCost((float)2990.5396);
-		client.setCorrectRate((float)97.066666);
-		client.setTotal(375);
+		client.setAvgCost((float)2937.778);
+		client.setCorrectRate((float)96.15115);
+		client.setTotal(1429);
 		System.out.println(score(client));
 	}
 }
